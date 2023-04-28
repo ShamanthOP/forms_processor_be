ff7441f722555172bd71e573f42df72b3f964fef
Shamanth <shamanth1604@gmail.com>
2023-04-28 21:31:59 +0530

    Added docker and vscode config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello world");
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "message",
+    });
+});
+
+const port = Number(process.env.PORT || 8080);
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
